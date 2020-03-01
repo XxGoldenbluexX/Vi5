e3c25d626645d58d42e485d6ea83d851c39f47c9
XxGoldenbluexX <tonyg222@outlook.fr>
2020-03-01 13:47:55 +0100

    Ajout de Vi5CommandBase
    package command et de la commande vi5 dans le plugin.yml

--- Vi5/src/fr/vi5team/vi5/Vi5Main.java
+++ Vi5/src/fr/vi5team/vi5/Vi5Main.java
@@ -2,14 +2,17 @@ package fr.vi5team.vi5;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.vi5team.vi5.commands.Vi5BaseCommand;
+
 public class Vi5Main extends JavaPlugin {
 	
 	private ConfigManager cfgmanager;
 	
 	@Override
 	public void onEnable() {
-		cfgmanager = new ConfigManager(this);
 		super.onEnable();
+		cfgmanager = new ConfigManager(this);
+		getCommand("vi5").setExecutor(new Vi5BaseCommand());
 	}
 	public ConfigManager getCfgmanager() {
 		return cfgmanager;
