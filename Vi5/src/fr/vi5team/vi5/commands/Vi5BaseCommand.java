81cb990977b374dc6813a01d70c16ac46dd0bd52
pctotitou <pctotitou@pctotitou-PC>
2020-03-07 20:07:58 +0100

    base command switch

--- Vi5/src/fr/vi5team/vi5/commands/Vi5BaseCommand.java
+++ Vi5/src/fr/vi5team/vi5/commands/Vi5BaseCommand.java
@@ -6,6 +6,32 @@ public class Vi5BaseCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		switch (arg3[0]) {
+		case "game":
+			gameCommand(arg0, arg1, arg2, arg3);
+		case "map":
+			mapCommand(arg0, arg1, arg2, arg3);
+		}
 		return false;
+	}
+	
+	public void gameCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		switch (arg3[1]) {
+		case "create":
+		case "start":
+		case "restart":
+		case "stop":
+		case "join":
+		case "leave":
+		case "list":
+		}
+	}
+	
+	public void mapCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		switch (arg3[1]) {
+		case "create":
+		case "rename":
+		case "list":
+		}
 	}
 }
