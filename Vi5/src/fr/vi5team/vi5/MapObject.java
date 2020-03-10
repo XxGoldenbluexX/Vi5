979250ac668008eeb2e68bdc497d66c198093634
pctotitou <pctotitou@pctotitou-PC>
2020-03-10 19:09:35 +0100

    Mis le code en commentaire car rien dessus

--- Vi5/src/fr/vi5team/vi5/MapObject.java
+++ Vi5/src/fr/vi5team/vi5/MapObject.java
@@ -3,5 +3,27 @@ package fr.vi5team.vi5;
 import org.bukkit.event.Listener;
 
 public class MapObject implements Listener {
-
+	/*
+	private Game game = new Game(null, null);
+	public boolean playerInAnyObjectRangeChecker() {
+		POUR CHAQUE GAME CREER ON TEST SI ELLE EST EN COURS
+		if(game.is_Started()) {
+			playerInAnyObjectRange(String map_name)
+		}
+		return false;
+	}
+	public void playerInAnyObjectRange(String map_name) {
+		if(true) {
+			return player, map_name, object_name;
+		}
+		else {
+			return null;
+		}
+	}
+	public void pointCapture() {
+		if(playerInAnyObjectRangeChecker) {
+			
+		}
+	}
+	*/
 }
