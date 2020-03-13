fd01c59e50c8ce37b43d88568691efb8d81890a1
pctotitou <pctotitou@pctotitou-PC>
2020-03-13 23:18:58 +0100

    mapobject player in zone test. Rajouter la var objectName dans Game

--- Vi5/src/fr/vi5team/vi5/MapObject.java
+++ Vi5/src/fr/vi5team/vi5/MapObject.java
@@ -1,37 +1,30 @@
 package fr.vi5team.vi5;
 
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class MapObject implements Listener {
 	private Player player;
 	private String object_name;
 	private ConfigManager ConfigManager = new ConfigManager(null);
 	private Game gameref;
+
 	
-	@SuppressWarnings("unused")
-	public List<Object> playerInObjectRange(String mapname) {
-		YamlConfiguration mapconfig = ConfigManager.getMapConfig(mapname);
-		if(player.getLocation() != null) {
-			return Arrays.asList(player, mapname, object_name);
-		}
-		else {
-			return null;
+	public void onPlayerMove(PlayerMoveEvent event, String _objectName, Location _position, int sizex,int sizey,int sizez) {
+		Location loc = event.getTo();
+		if(loc.getBlockX()>=_position.getBlockX()&&(loc.getBlockX()<=_position.getBlockX()+sizex)&&loc.getBlockY()>=_position.getBlockY()&&loc.getBlockY()<=_position.getBlockY()+sizey&&loc.getBlockZ()>=_position.getBlockZ()&&loc.getBlockZ()<=_position.getBlockZ()+sizez){
+			//code here
 		}
 	}
-	public void pointCapture() {	
-		
-	}
-	public MapObject(Game game, Location _position, Location _blockPosition,BlockData _blockData,Material _blocType,int sizex,int sizey,int sizez) {
+	public MapObject(Game game, String _objectName, Location _position, Location _blockPosition,BlockData _blockData,Material _blocType,int sizex,int sizey,int sizez) {
 		gameref = game;
+		
+		
 		
 	}
 }
