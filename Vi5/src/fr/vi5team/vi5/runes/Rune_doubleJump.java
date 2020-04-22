5a10e1c564d5a858c3515ec659878b8f43fc7d12
XxGoldenbluexX <tonyg222@outlook.fr>
2020-04-22 20:04:36 +0200

    J'aime les modifers

---
+++ Vi5/src/fr/vi5team/vi5/runes/Rune_doubleJump.java
@@ -0,0 +1,60 @@
+package fr.vi5team.vi5.runes;
+
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+
+import fr.vi5team.vi5.PlayerWrapper;
+import fr.vi5team.vi5.Vi5Main;
+import fr.vi5team.vi5.enums.RunesList;
+
+public class Rune_doubleJump extends BaseRune {
+	
+	private boolean canDoubleJump=false;
+
+	public Rune_doubleJump(Vi5Main _mainref, PlayerWrapper _wraper, Player _player, RunesList _rune) {
+		super(_mainref, _wraper, _player, _rune);
+	}
+
+	@EventHandler
+	public void onToogleFly(PlayerToggleFlightEvent event) {
+		if (event.getPlayer().equals(player)) {
+			event.setCancelled(true);
+			if (canDoubleJump) {
+				player.setVelocity(player.getLocation().getDirection().multiply(0.1).setY(0.1));
+				player.playSound(player.getLocation(),Sound.ITEM_FIRECHARGE_USE, SoundCategory.AMBIENT, 0.3f, 1.5f);
+				player.playSound(player.getLocation(),Sound.ITEM_HOE_TILL, SoundCategory.AMBIENT, 1f, 0.1f);
+				canDoubleJump=false;
+			}
+		}
+	}
+	
+	@Override
+	public void cast() {
+	}
+
+	@Override
+	public void tick() {
+		if (player.isOnGround()) {
+			canDoubleJump=true;
+		}
+	}
+
+	@Override
+	public void gameEnd() {
+		player.setAllowFlight(false);
+	}
+
+	@Override
+	public void gameStart() {
+	}
+
+	@Override
+	public void enterZone() {
+		Activate();
+		player.setAllowFlight(true);
+	}
+
+}
--- Vi5/src/fr/vi5team/vi5/runes/Rune_tough.java
+++ Vi5/src/fr/vi5team/vi5/runes/Rune_tough.java
@@ -1,9 +1,8 @@
 package fr.vi5team.vi5.runes;
 
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Player;
-
-import com.sun.xml.internal.stream.Entity;
 
 import fr.vi5team.vi5.PlayerWrapper;
 import fr.vi5team.vi5.Vi5Main;
@@ -11,6 +10,8 @@ import fr.vi5team.vi5.enums.RunesList;
 
 public class Rune_tough extends BaseRune {
 
+	private AttributeModifier modifier;
+	
 	public Rune_tough(Vi5Main _mainref, PlayerWrapper _wraper, Player _player, RunesList _rune) {
 		super(_mainref, _wraper, _player, _rune);
 		// TODO Auto-generated constructor stub
@@ -30,7 +31,7 @@ public class Rune_tough extends BaseRune {
 
 	@Override
 	public void gameEnd() {
-		player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20);
+		player.getAttribute(Attribute.GENERIC_MAX_HEALTH).removeModifier(modifier);
 		player.setHealth(20);
 		// TODO Auto-generated method stub
 		
@@ -44,7 +45,8 @@ public class Rune_tough extends BaseRune {
 	@Override
 	public void enterZone() {
 		Activate();
-		player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(24);
+		modifier=new AttributeModifier("Rune_tough",4,AttributeModifier.Operation.ADD_NUMBER);
+		player.getAttribute(Attribute.GENERIC_MAX_HEALTH).addModifier(modifier);
 		player.setHealth(24);
 		// TODO Auto-generated method stub
 		
