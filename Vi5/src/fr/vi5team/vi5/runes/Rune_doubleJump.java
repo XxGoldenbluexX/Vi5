1a3f1c0256fadd781ce5031b0c0051b1258fb63a
XxGoldenbluexX <tonyg222@outlook.fr>
2020-04-23 22:31:14 +0200

    Double jump fly bug

--- Vi5/src/fr/vi5team/vi5/runes/Rune_doubleJump.java
+++ Vi5/src/fr/vi5team/vi5/runes/Rune_doubleJump.java
@@ -12,13 +12,15 @@ import fr.vi5team.vi5.enums.RunesList;
 
 public class Rune_doubleJump extends BaseRune {
 	
+	private boolean canDoubleJump=false;
+	
 	public Rune_doubleJump(Vi5Main _mainref, PlayerWrapper _wraper, Player _player, RunesList _rune) {
 		super(_mainref, _wraper, _player, _rune);
 	}
 
 	@EventHandler
 	public void onToogleFly(PlayerToggleFlightEvent event) {
-		if (event.getPlayer().equals(player)) {
+		if (event.getPlayer().equals(player) && canDoubleJump) {
 			event.setCancelled(true);
 			player.setVelocity(player.getVelocity().setY(0.5));
 			player.playSound(player.getLocation(),Sound.ITEM_FIRECHARGE_USE, SoundCategory.AMBIENT, 0.3f, 1.5f);
@@ -33,7 +35,7 @@ public class Rune_doubleJump extends BaseRune {
 
 	@Override
 	public void tick() {
-		if (player.isOnGround()) {
+		if (player.isOnGround() && canDoubleJump) {
 			player.setAllowFlight(true);
 		}
 	}
@@ -50,6 +52,7 @@ public class Rune_doubleJump extends BaseRune {
 	@Override
 	public void enterZone() {
 		Activate();
+		canDoubleJump=true;
 	}
 
 }
