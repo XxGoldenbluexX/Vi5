d40052034cfe0fddd864f9418f51d8e334be98fc
pctotitou <pctotitou@pctotitou-PC>
2020-04-23 22:34:50 +0200

    up

--- Vi5/src/fr/vi5team/vi5/runes/Rune_crocheteur.java
+++ Vi5/src/fr/vi5team/vi5/runes/Rune_crocheteur.java
@@ -10,37 +10,31 @@ public class Rune_crocheteur extends BaseRune{
 
 	public Rune_crocheteur(Vi5Main _mainref, PlayerWrapper _wraper, Player _player, RunesList _rune) {
 		super(_mainref, _wraper, _player, _rune);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void cast() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void tick() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void gameEnd() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void gameStart() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void enterZone() {
 		Activate();
-		// TODO Auto-generated method stub
 		
 	}
 
