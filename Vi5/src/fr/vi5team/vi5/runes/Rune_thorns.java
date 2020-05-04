572614584f5120ee9ecfdd484271fc488e25e2bc
pctotitou <pctotitou@pctotitou-PC>
2020-05-04 20:04:34 +0200

    rune thorn fixed?

--- Vi5/src/fr/vi5team/vi5/runes/Rune_thorns.java
+++ Vi5/src/fr/vi5team/vi5/runes/Rune_thorns.java
@@ -1,7 +1,6 @@
 package fr.vi5team.vi5.runes;
 
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -22,14 +21,16 @@ public class Rune_thorns extends BaseRune{
 		if(wraper.getGame().getVoleurInsideList().size()>1) {
 			Entity damager = event.getDamager();
 			Entity receiver = event.getEntity();
-			if(damager.getType()==EntityType.PLAYER&&receiver.getType()==EntityType.PLAYER){
+			if(damager instanceof Player && receiver instanceof Player) {
 				Player pDamager = (Player)damager;
 				Player pReceiver = (Player)receiver;
-				PlayerWrapper receiverWrap = mainref.getPlayerWrapper(pReceiver);
-				if(pDamager.getName()==player.getName()&&receiverWrap.getTeam()==Vi5Team.VOLEUR) {
-					for(Player thief : wraper.getGame().getVoleurInsideList()) {
-						if(thief!=pReceiver) {
-							thief.damage(0);
+				if(mainref.getPlayerWrapper(pDamager).is_ingame()&&mainref.getPlayerWrapper(pReceiver).is_ingame()) {
+					PlayerWrapper receiverWrap = mainref.getPlayerWrapper(pReceiver);
+					if(pDamager==player&&receiverWrap.getTeam()==Vi5Team.VOLEUR) {
+						for(Player thief : wraper.getGame().getVoleurInsideList()) {
+							if(thief!=pReceiver) {
+								thief.damage(0.01);
+							}
 						}
 					}
 				}
